97a645d42aeb7d0043e4d2ead25766c4312b72fa
Eric Ponce <spc525nc1@gmail.com>
2025-02-04 07:39:01 -0500

    update 2

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 import express from 'express'
 import { ConexionMongo } from "./Model/conexion.js";
+import { Schema, model } from 'mongoose';
+
 
 ConexionMongo()
+const SchemaNuevo = new Schema({
+    name: String,
+    direccion: String,
+    appelido: String
+}, {
+    versionKey: false
+})
+
+const baseMongo = new model('Paginas', SchemaNuevo)
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const data = await baseMongo.find()
+    res.json(data)
+})
+
+
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:3000`)
+})
+
